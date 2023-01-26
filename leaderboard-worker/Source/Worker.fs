6dc028198ded650daacc53bcce53f4bd5e8f2525
Gintautas Kazlauskas <g@veil.gg>
2023-01-26 03:36:52 +0200

    (worker): Adding stubs for gRPC connection

--- leaderboard-worker/Source/Worker.fs
+++ leaderboard-worker/Source/Worker.fs
@@ -5,11 +5,16 @@ open System.Threading
 open System.Threading.Tasks
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.Logging
+open Google.Protobuf
+open Grpc.Net.Client
 
 type LeaderboardWorker(logger: ILogger<LeaderboardWorker>, account: Account) =
     interface IHostedService with
         member _.StartAsync(ct: CancellationToken) =
             task {
+                use channel = GrpcChannel.ForAddress("http://localhost:8081/")
+                let client = Airdog.AirdogService.AirdogServiceClient(channel)
+
                 let helper = new SteamLeaderboardHelper(account, "LEVELS", ct)
                 logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now)
                 helper.Connect
@@ -17,6 +22,7 @@ type LeaderboardWorker(logger: ILogger<LeaderboardWorker>, account: Account) =
                 let leaderboards = helper.GetEntries
 
                 for leaderboard in leaderboards do
+                    let req = { LeaderboardId = leaderboard.ID; Entry = leaderboard.Entries }
                     printfn $"Entries for leaderboard {leaderboard.ID}"
                     for entry in leaderboard.Entries do
                         printfn $"SteamID: {entry.SteamID.ConvertToUInt64()} - {entry.Score}ms"
